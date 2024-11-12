e477c3304369cd007a295e01ba804c1c24ae9856
Robson Alves <contato@robsonalves.dev.br>
2024-11-12 15:06:27 -0300

    Add route configuration for user management API

--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -1,11 +1,26 @@
 package rotas
 
-import "net/http"
+import (
+	"net/http"
 
-// Rota representa uma rota da API
+	"github.com/gorilla/mux"
+)
+
+// Rota - Representa uma rota da API
 type Rota struct {
 	URI                string
 	Metodo             string
 	Funcao             func(http.ResponseWriter, *http.Request)
 	RequerAutenticacao bool
+}
+
+// Configurar - Configura as rotas da API
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	}
+
+	return r
 }
--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,8 +1,13 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"api/src/router/rotas"
 
-// Gerar retorna um router com as rotas configuradas
+	"github.com/gorilla/mux"
+)
+
+// Gerar - Retorna um router com as rotas configuradas
 func Gerar() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+	return rotas.Configurar(r)
 }
