f63a1503a0aa3ad8e30333985c13d0de8910ccc0
Robson Alves <contato@robsonalves.dev.br>
2024-11-12 17:07:11 -0300

    Implement user creation functionality with JSON parsing and error handling

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -1,10 +1,24 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/modelos"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
 
 // CriarUsuario - Cria um usuário
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuário"))
+	corpoRequest, erro := io.ReadAll(r.Body)
+	if erro == nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro := json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
 }
 
 // BuscarUsuarios - Busca todos os usuários
