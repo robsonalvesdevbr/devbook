c977f86437a26c97ee858fdbf655efd6a7fdde43
Robson Alves <contato@robsonalves.dev.br>
2024-11-12 15:50:49 -0300

    Refactor user controller comments for consistency and clarity

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -2,27 +2,27 @@ package controllers
 
 import "net/http"
 
-// CriarUsuario cria um usuário
+// CriarUsuario - Cria um usuário
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Criando usuário"))
 }
 
-// BuscarUsuarios busca todos os usuários
+// BuscarUsuarios - Busca todos os usuários
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Buscar usuários"))
 }
 
-// BuscarUsuario busca um usuário
+// BuscarUsuario - Busca um usuário
 func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Buscar usuário"))
 }
 
-// AtualizarUsuario atualiza um usuário
+// AtualizarUsuario - Atualiza um usuário
 func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Atualizar usuário"))
 }
 
-// DeletarUsuario deleta um usuário
+// DeletarUsuario - Deleta um usuário
 func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Deletar usuário"))
 }
