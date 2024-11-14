9fc4b800ab84ecbc76632719e9e4de53c39fa649
Robson Alves <contato@robsonalves.dev.br>
2024-11-14 17:32:15 -0300

    Add user preparation methods for validation and formatting in Usuario model

--- api/src/modelos/Usuario.go
+++ api/src/modelos/Usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario - Estrutura que representa um usuário do sistema
 type Usuario struct {
@@ -10,4 +14,41 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criado_em,omitempty"`
+}
+
+// validar - Método para chamar os métodos de validação e formatação do usuário
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O nick é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O email é obrigatório e não pode estar em branco")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("A senha é obrigatória e não pode estar em branco")
+	}
+
+	return nil
+}
+
+// formatar - Método para chamar o método de formatação do usuário
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
+}
+
+// Preparar - Método para chamar os métodos de formatação e validação do usuário
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+	usuario.formatar()
+	return nil
 }
