01dc3e8935eee57002e15a3dbff623ce9058a3e2
Robson Alves <contato@robsonalves.dev.br>
2024-11-19 13:39:19 -0300

    Fix JSON response header setting order in respostas.go

--- api/src/respostas/respostas.go
+++ api/src/respostas/respostas.go
@@ -8,8 +8,8 @@ import (
 
 // JSON - Retorna um JSON para a requisição
 func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
-	w.WriteHeader(statusCode)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
 	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
 		log.Fatal(erro)
 	}
